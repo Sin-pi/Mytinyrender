6c7d8303f46743bfd84f23dac5b08f7ea930204f
Blake Padrones <padronesblake@yahoo.com>
2024-03-24 12:37:07 -0500

    I think removed sub .git folder

---
+++ tinyrenderer/main.cpp
@@ -0,0 +1,8 @@
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAsColor color {
+  for(float t=)
+})
